bbfbf15ac4d75e89a4e6ddb62a046a3469f42fd4
edpark23 <edpark23@gmail.com>
2025-03-05 20:35:17 -0500

    Initial Commit

---
+++ RoundsVersion2/RoundsVersion2App.swift
@@ -0,0 +1,17 @@
+//
+//  RoundsVersion2App.swift
+//  RoundsVersion2
+//
+//  Created by Edward Park on 3/5/25.
+//
+
+import SwiftUI
+
+@main
+struct RoundsVersion2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RoundsVersion2Tests/RoundsVersion2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  RoundsVersion2Tests.swift
+//  RoundsVersion2Tests
+//
+//  Created by Edward Park on 3/5/25.
+//
+
+import Testing
+@testable import RoundsVersion2
+
+struct RoundsVersion2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
